227c9aad2c8c6bb9eb254b7ad038fc78a079b3ab
ymlihaa <ymlihaa@gmail.com>
2020-12-05 23:31:23 +0300

    base commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 8000,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
